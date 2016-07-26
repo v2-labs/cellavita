d1f8a27881851c2a765bef90a610c90405f458ed
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2016-07-25 23:00:59 -0300

    Add some more useful queries
    This commit adds some more useful queries to the sample file basic-queries.sql

--- support-files/basic-queries.sql
+++ support-files/basic-queries.sql
@@ -8,3 +8,6 @@ select * from vaccines;
 select * from addresses;
 select * from telephones;
 alter table `cellavita`.`cultures` add index `fk_cultures_cells1_idx` (`donor_id` ASC, `cell_id` ASC);
+select (max(`donor_id`)+1) as `next_id` from `cellavita`.`donors`;
+select (max(`address_id`)+1) as `donor_next_address_id` from `cellavita`.`addresses` where `donor_id`= 2;
+select (max(`phone_id`)+1) as `donor_next_telephone_id` from `cellavita`.`telephones` where `donor_id`= 1;
